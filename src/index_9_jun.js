0b5d89b22d0a1e59ca43c2e22b2cfbeca383e4b2
alirizvibuzzinteractive <ali.rizvi@buzz-interactive.com>
2023-06-20 14:16:43 +0500

    add updated code

--- src/index_9_jun.js
+++ src/index_9_jun.js
@@ -378,7 +378,7 @@ function init() {
   // cloth material
 
   var loader = new THREE.TextureLoader();
-  var clothTexture = loader.load(require("./textures/texturecloth5.jpeg"));
+  var clothTexture = loader.load(require("./textures/Cloth-Animation-3D-Final.jpeg"));
   clothTexture.anisotropy = 16;
 
   clothMaterial = new THREE.MeshLambertMaterial({
